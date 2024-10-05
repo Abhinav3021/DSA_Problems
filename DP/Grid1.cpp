a46370fc863556b4ebc943bd768d6083bed42e4a
Abhinav3021 <124438690+Abhinav3021@users.noreply.github.com>
2024-10-05 17:49:46 +0530

    05/10/2024

--- DP/Grid1.cpp
+++ DP/Grid1.cpp
@@ -1,17 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std ;
+const int Mod=1e9+7;
 int dp[1005][1005];
 char grid[1005][1005];
-vector <pair(int,int)> topoOrder;
+//int count=0;
 
-int dfs(int i,int j){
-    if()
-    if(grid[i][j]=='#'){return 0;}
-    dfs(i+1,j);
-    dfs(i,j+1);
+int func(int i,int j,int H,int W,int &count){
+    //base condition
+    if(i==H-1 && j==W-1){
+        count++;
+        dp[i][j]=count;
+        return count;
+    }
+    if(dp[i][j]!=-1){return dp[i][j];}
+    if(i>H-1 || j>W-1 || i<0 || j<0 || grid[i][j]=='#') return 0;
+    func(i+1,j,H,W,count);
+    func(i,j+1,H,W,count);
 }
-
-
 
 
 int main(){
@@ -23,6 +28,10 @@ int main(){
              cin >> grid[i][j];
         }
     }
+    int count=0;
+    func(0,0,H,W,count);
+    cout << count << '\n';
+    return count;
 
 }
 
